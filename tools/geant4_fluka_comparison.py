c4feec136440e9e712252d6dd6412ad573b9c825
Anton Taleiko <anton.taleiko@gmail.com>
2023-07-17 14:31:34 +0200

    Removed the regression line from the comparison graphs

--- tools/geant4_fluka_comparison.py
+++ tools/geant4_fluka_comparison.py
@@ -18,15 +18,15 @@ def main():
 def plotPyg4ometryFluka():
     x0, eta = getFlukaPyg4ometryResults()
     flukaColor = "blue"
-    sns.regplot(x=eta, y=x0, x_bins=50, color=flukaColor)
+    sns.regplot(x=eta, y=x0, x_bins=50, color=flukaColor, fit_reg=False)
 
 def plotPyg4ometryUpRoot():
     with uproot.open("data/geant4_root_files/geant4_material_tracks.root") as file:
         x0 = np.array(file["material-tracks/t_X0"].array())
         eta = np.array(file["material-tracks/v_eta"].array())
     plt.rcParams["figure.autolayout"] = True
-    # sns.regplot(x=eta, y=x0, x_bins=20, marker='o', fit_reg=True)
-    sns.regplot(x=eta, y=x0, x_bins=50, color="orange")
+    # sns.regplot(x=eta, y=x0, x_bins=20, marker='o', fit_reg=False)
+    sns.regplot(x=eta, y=x0, x_bins=50, color="orange", fit_reg=False)
 
 def getFlukaPyg4ometryResults():
     df = pd.read_csv(FLUKA_PYG4OMETRY_RESULTS)
