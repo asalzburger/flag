05065556395b8175601258c4439de0e1fe0032d0
Anton Taleiko <anton.taleiko@gmail.com>
2023-08-11 15:30:44 +0200

    Completed the material mapping inspection tool

--- tools/material_mapping_inspector.py
+++ tools/material_mapping_inspector.py
@@ -2,13 +2,18 @@ import sys
 import uproot
 import numpy as np
 
-MATERIAL_RECORDING_RESULTS = "data/closing_the_loop/geant4_material_tracks.root"
-MATERIAL_RANGE_OUTPUT_FILE = "data/closing_the_loop/range.txt"
+# MATERIAL_RECORDING_RESULTS = "data/closing_the_loop/geant4_material_tracks.root"
+# MATERIAL_RECORDING_RESULTS = "$HOME/builds/acts/Examples/Scripts/Python/geant4_material_tracks.root"
+MATERIAL_RECORDING_RESULTS = "geant4_material_tracks.root"
+# MATERIAL_RANGE_OUTPUT_FILE = "data/closing_the_loop/range.txt"
+MATERIAL_RANGE_OUTPUT_FILE = "$HOME/Documents/CERN/Arbete/flag/data/closing_the_loop/range.txt"
+# MATERIALS_OUTPUT_FILE = "$HOME/Documents/CERN/Arbete/flag/data/closing_the_loop/materials.txt"
+MATERIALS_OUTPUT_FILE = "/home/taleiko/Documents/CERN/Arbete/flag/data/closing_the_loop/materials.txt"
 
 def main():
     matZ = readMaterialRecordings()
     materialArr = createSortedMaterialArray(matZ)
-    outputMaterialRange(materialArr)
+    outputMaterials(materialArr)
 
 def readMaterialRecordings(filePath=MATERIAL_RECORDING_RESULTS):
     with uproot.open(filePath) as file:
@@ -17,17 +22,16 @@ def readMaterialRecordings(filePath=MATERIAL_RECORDING_RESULTS):
 
 def createSortedMaterialArray(matZ):
     materials = set()
-    for materialList in matZ:
-        for material in materialList:
+    for materialArr in matZ:
+        for material in materialArr:
             materials.add(material)
     materialArr = np.array(list(materials))
     materialArr = np.sort(materialArr)
     return materialArr
 
-def outputMaterialRange(materialArr, outputFile=MATERIAL_RANGE_OUTPUT_FILE):
-    outputString = " ".join([str(materialArr[0]), str(materialArr[len(materialArr)-1])])
+def outputMaterials(materialArrs, outputFile=MATERIALS_OUTPUT_FILE):
     with open(outputFile, "w") as f:
-        f.write(outputString)
+        f.write(" ".join([str(material) for material in materialArrs]))
 
 
 if __name__ == '__main__':
