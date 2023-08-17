db89175a380c1d3a64459cb5e0c1d4dc5fb1de8f
Anton Taleiko <anton.taleiko@gmail.com>
2023-08-17 14:55:00 +0200

    Added a script that sets up the gitignored directories containing data.

---
+++ scripts/directory_setup.sh
@@ -0,0 +1,9 @@
+mkdir data
+mkdir data/closing_the_loop
+mkdir cylinder_fluka
+mkdir cylinder_g4
+mkdir fluka_material_database
+mkdir g4_material_database
+mkdir geant4_root_files
+# The file geant4_material_tracks.root in acts need to be linked to the directory geant4_root_files
+mkdir open_data_detector
