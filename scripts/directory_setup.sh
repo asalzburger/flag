3eaa13b8e59811c71da764fc16a47a1130b3df3d
Anton Taleiko <anton.taleiko@gmail.com>
2023-08-18 10:50:33 +0200

    Corrected a spelling error

--- scripts/directory_setup.sh
+++ scripts/directory_setup.sh
@@ -5,5 +5,5 @@ mkdir data/cylinder_g4
 mkdir data/fluka_material_database
 mkdir data/g4_material_database
 mkdir data/geant4_root_files
-# The file geant4_material_tracks.root in acts need to be linked to the directory geant4_root_files
+# The file geant4_material_tracks.root in acts needs to be linked to the directory geant4_root_files
 mkdir data/open_data_detector
